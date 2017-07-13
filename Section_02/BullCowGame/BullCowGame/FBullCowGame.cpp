316afc6b63f9f8d03f5f9c70887e31c7676c7a8b
Bruno Paixao <brunomrpx@gmail.com>
2017-07-12 22:53:14 -0300

    [Session 2 - Lecture 35] Added struct for Bulls and Cows control.

--- Section_02/BullCowGame/BullCowGame/FBullCowGame.cpp
+++ Section_02/BullCowGame/BullCowGame/FBullCowGame.cpp
@@ -20,8 +20,10 @@ int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
 
 void FBullCowGame::Reset() {
     constexpr int32 MAX_TRIES = 8;
-    MyMaxTries = MAX_TRIES;
+    const FString HIDDEN_WORD = "planet";
     
+    MyMaxTries = MAX_TRIES;
+    MyHiddenWord = HIDDEN_WORD;
     MyCurrentTry = 1;
     
     return;
@@ -33,5 +35,19 @@ bool FBullCowGame::IsGameWon() const {
 
 bool FBullCowGame::CheckGuessValidity(FString Guess) {
     return true;
+}
+
+// receives a VALID guess, increments turn, and returns count
+BullCowCount FBullCowGame::SubmitGuess(FString) {
+    // increment the turn number
+    MyCurrentTry++;
+    
+    // setup a return variable
+    BullCowCount BullCowCount;
+    
+    // loop through all letters in the guess
+        // compare letters against the hidden word
+
+    return BullCowCount;
 }
 
--- Section_02/BullCowGame/BullCowGame/FBullCowGame.hpp
+++ Section_02/BullCowGame/BullCowGame/FBullCowGame.hpp
@@ -11,6 +11,12 @@
 using FString = std::string;
 using int32 = int;
 
+// all values initialised to zero
+struct BullCowCount {
+    int32 Bulls = 0;
+    int32 Cows = 0;
+};
+
 class FBullCowGame {
 public:
     FBullCowGame(); // constructor
@@ -21,10 +27,13 @@ public:
     
     void Reset(); // TODO make a more rich return value
     bool CheckGuessValidity(FString); // TODO make a more rich return value
-    // provide a method for counting bulls & cows, and increaseing try number
+    
+    // counts bulls & cows and increases try number assuming valid guess
+    BullCowCount SubmitGuess(FString);
     
 private:
     int32 MyCurrentTry;
     int32 MyMaxTries;
+    FString MyHiddenWord;
 };
 
